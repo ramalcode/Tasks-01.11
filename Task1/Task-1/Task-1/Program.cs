946a9a2db4fd396deabadb0bc7bc53c4056f0f96
ramalfatullayevh <tu7ma9clt@code.edu.az>
2022-11-01 17:21:48 +0400

    added all c# and js tasks

---
+++ Task1/Task-1/Task-1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = { 21, 34, 20, 47, 49 };
+int min;
+min = numbers[0];
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i]<min)
+    {
+        min = numbers[i];
+    }
+
+}
+Console.WriteLine(min);
---
+++ Task2/Task2/Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = { 33, 4, 76, 89, 5, 2, 11, 7 };
+int reqem=0;
+for (int i = 0; i < numbers.Length; i++)
+{
+	if (numbers[i]<10)
+	{
+		reqem++;
+	}
+}
+Console.WriteLine(reqem);
